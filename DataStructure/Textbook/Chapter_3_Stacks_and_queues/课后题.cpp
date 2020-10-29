6a18566e3baf50c61b23f5d25215bfd806d58ca4
jancoyan <53427402+jancoyan@users.noreply.github.com>
2020-10-29 23:22:58 +0800

    update exercise

--- DataStructure/Textbook/Chapter_3_Stacks_and_queues/课后题.cpp
+++ DataStructure/Textbook/Chapter_3_Stacks_and_queues/课后题.cpp
@@ -133,6 +133,14 @@ bool isPlalindrome(string str) {
 }
 
 
+bool f(string str) {
+	for (int i = 0; i < str.length()/2; i++)
+		if (str[i] != str[str.length() - 1 - i])
+			return false;
+	return true;
+}
+
+
 //2.3
 
 void mystack() {
