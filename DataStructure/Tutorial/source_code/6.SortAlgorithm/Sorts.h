6e1122a554a44ce725267deda311e4ae801688c5
jancoyan <53427402+jancoyan@users.noreply.github.com>
2020-12-15 00:41:45 +0800

    tutorial - 6 upload

---
+++ DataStructure/Tutorial/source_code/6.SortAlgorithm/Sorts.h
@@ -0,0 +1,34 @@
+#pragma once
+#include<vector>
+using namespace std;
+class Sorts {
+
+public:
+
+	Sorts() {
+		this->cmpOfQuickSort = 0;
+		this->movOfQuickSort = 0;
+		this->cmpOfHeapSort = 0;
+		this->movOfHeapSort = 0;
+	}
+
+	vector<int> bubbleSort(int* nums, int n); //√∞≈›≈≈–Ú
+
+	vector<int> insertSort(int*, int); // ≤Â»Î≈≈–Ú
+
+	vector<int> selectionSort(int*, int); // —°‘Ò≈≈–Ú
+
+	vector<int> shellSort(int*, int); // œ£∂˚≈≈–Ú
+	
+	void quickSort(int*, int, int); // øÏ≈≈
+
+	void heapSort(int*, int); // ∂—≈≈–Ú
+
+	void heapAdjust(int*, int, int);
+
+public:
+	int cmpOfQuickSort;
+	int movOfQuickSort;
+	int cmpOfHeapSort;
+	int movOfHeapSort;
+};
