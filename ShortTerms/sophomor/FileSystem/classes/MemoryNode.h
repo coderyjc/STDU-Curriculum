d698703b83d4254b5a9c9beac42070b7a3678e4b
jancoyan <302848188@qq.com>
2021-06-27 21:53:17 +0800

    update

---
+++ ShortTerms/sophomor/FileSystem/classes/MemoryNode.h
@@ -0,0 +1,20 @@
+//
+// Created by YanJingcun on 2021/6/27.
+//
+
+#ifndef FILESYSTEM_MEMORYNODE_H
+#define FILESYSTEM_MEMORYNODE_H
+
+
+class MemoryNode {
+public:
+
+
+
+private:
+
+
+};
+
+
+#endif //FILESYSTEM_MEMORYNODE_H
---
+++ ShortTerms/sophomor/FileSystem/cmake-build-debug/CMakeFiles/FileSystem.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "R:/GITHUB/Curriculum/ShortTerms/sophomor/FileSystem/classes/MemoryNode.cpp" "R:/GITHUB/Curriculum/ShortTerms/sophomor/FileSystem/cmake-build-debug/CMakeFiles/FileSystem.dir/classes/MemoryNode.cpp.obj"
+  "R:/GITHUB/Curriculum/ShortTerms/sophomor/FileSystem/main.cpp" "R:/GITHUB/Curriculum/ShortTerms/sophomor/FileSystem/cmake-build-debug/CMakeFiles/FileSystem.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
