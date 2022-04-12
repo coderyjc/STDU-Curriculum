43236046daa60fa17e84db912f51487fddf8536b
jancoyan <302848188@qq.com>
2022-04-13 07:49:01 +0800

    update dispatch algorithm

--- OS/ProcessDispatch/ProcessDispatch/main.cpp
+++ OS/ProcessDispatch/ProcessDispatch/main.cpp
@@ -1,12 +1,13 @@
 /*********************************************
-* 
+*
 *	Author: Yan Jingcun
 *	Date: 2022.04.05
 *	Time: 13:10
 *	Description: 模拟优先级调度和时间片轮转调度算法
-* 
+*
 **********************************************/
 
+#include<stdlib.h>
 #include<iostream>
 #include<algorithm>
 #include<vector>
@@ -23,16 +24,31 @@ using namespace std;
 // 进程处于完成状态
 #define FINISH_STATE 2
 
+// 当前算法的名称, 也是优先级调度所需的时间片的数量
+#define PRIORITY_ALGORITHM 1
+
+// 当前算法的名称, 也是时间片轮转所需的时间片的数量
+#define ROLL_ALGORITHM 2
+
+// 当前使用的算法, 也作为当前算法使用时间片的数量
+int current_dispatch_algorithm = 1;
+
+// 时间片轮转算法中的时间片大小
+int time_slice = 2;
+
+// 当前的CPU总时间
+int runTime = 0;
+
 /// <summary>
 /// 进程控制块
 /// </summary>
-typedef struct PCB{
+typedef struct PCB {
 
 	PCB() {}
 
 	PCB(string name, int prio, int cputime, int needtime, int status, PCB* nxt) {
 		this->p_name = name;
-		this->p_cputime = cputime; 
+		this->p_cputime = cputime;
 		this->next = nxt;
 		this->p_needtime = needtime;
 		this->p_priority = prio;
@@ -69,39 +85,81 @@ PCB* p_tail = NULL;
 PCB* p_finish = NULL;
 
 
-// 在优先数算法中，将尚未完成的 PCB 按优先数顺序插入到就绪队列中
-int insert1() {
 
+
+/// <summary>
+/// 在优先数算法中，将尚未完成的 PCB 按优先数顺序插入到就绪队列中
+/// </summary>
+/// <returns>正常返回0</returns>
+int insert1() {
 	PCB* temp = p_ready;
+
 	if (NULL == p_ready) {
+		// 在最后一个进程运行的时候, ready是空的, 但run不是空的
 		p_ready = p_run;
-	} else if (p_run->p_priority <= temp->p_priority) {
+	}
+	else if (p_run->p_priority <= temp->p_priority) {
+		// 按照优先级插入
 		p_run->next = p_ready;
 		p_ready = p_run;
 	}
 	else {
+		// 按照优先级插入
 		while (temp->next && (temp->p_priority < p_run->p_priority)) temp = temp->next;
 		p_run->next = temp->next;
 		temp->next = p_run;
 	}
-	if (NULL != p_tail) p_tail = p_tail->next;
-	if(FINISH_STATE != p_run->p_status) p_run->p_status = RUN_STATE;
+	if (NULL != p_tail->next) p_tail = p_tail->next;
+	if (FINISH_STATE != p_run->p_status) p_run->p_status = RUN_STATE;
 	else p_run->p_status = WAIT_STATE;
 	p_run = NULL;
 	return 0;
 }
 
-// 在轮转法中，将执行了一个时间片单位（为 2），但尚未完成的进程的 PCB，插到就绪队列的队尾；
+/// <summary>
+/// 在轮转法中，将尚未完成的 PCB 按优先数顺序插入到就绪队列 中； 
+/// </summary>
+/// <returns></returns>
 int insert2() {
+	// 直接放到队尾
+	if (NULL != p_tail->next) p_tail = p_tail->next;
+	p_tail->next = p_run;
+	p_tail = p_run; // 后移
+	p_run = NULL;
+	p_tail->next = NULL;
+	p_ready->p_status = RUN_STATE; //对头运行
 	return 0;
 }
 
-// 调度就绪队列的第一个进程投入运行；
+/// <summary>
+/// 调度就绪队列的第一个进程投入运行；
+/// </summary>
+/// <returns>正常返回0</returns>
 int firstIn() {
+	// cout << p_run->p_name << " ";
+	// cout << runTime << " ";
+
 	// 运行一个cpu时间
-	p_run->p_priority -= 1;
-	p_run->p_cputime += 1;
-	p_run->p_needtime -= 1;
+	if (current_dispatch_algorithm == PRIORITY_ALGORITHM) {
+		// 优先级调度
+		p_run->p_priority += 1;
+		p_run->p_cputime += 1;
+		p_run->p_needtime -= 1;
+		runTime += 1;
+	}
+	else if (current_dispatch_algorithm == ROLL_ALGORITHM) {
+		// 轮询
+		if (p_run->p_needtime == 1) {
+			p_run->p_needtime -= 1;
+			p_run->p_cputime += 1;
+			runTime += 1;
+		}
+		else {
+			p_run->p_needtime -= time_slice;
+			p_run->p_cputime += time_slice;
+			runTime += time_slice;
+		}
+	}
 	// 判断有没有运行结束
 	if (0 == p_run->p_needtime) {
 		// 运行完了, 插入完成队列
@@ -113,7 +171,7 @@ int firstIn() {
 		}
 		else {
 			PCB* temp = p_finish;
-			while(temp->next) temp = temp->next;
+			while (temp->next) temp = temp->next;
 			temp->next = p_run;
 			p_run->next = NULL;
 			p_run = NULL;
@@ -121,9 +179,10 @@ int firstIn() {
 	}
 	else {
 		// 没有运行完, 就插入到就绪队列
-		insert1();
+		current_dispatch_algorithm == PRIORITY_ALGORITHM ? insert1() : insert2();
 	}
-
+	// 进程开始执行的时间
+	// cout << runTime << "\n";
 	return 0;
 }
 
@@ -146,7 +205,6 @@ void subPrint(PCB* target) {
 
 /// <summary>
 /// 显示每执行一次后所有进程的状态及有关信息。
-/// --todo-- 还没写排队...在"W"态中，以优先数高低或轮转顺序排队；在"F"态中，以完成先后顺序排队。
 /// </summary>
 /// <returns></returns>
 int print() {
@@ -167,7 +225,7 @@ int print() {
 /// <returns>-1异常退出, 0正常退出</returns>
 int create() {
 	// 信息录入
-	cout << "进程名称 完成需要的时间片数\n";
+	cout << "进程名称 进程需要的cpu时间\n";
 	string t_name;
 	int t_needTime;
 	cin >> t_name >> t_needTime;
@@ -181,7 +239,10 @@ int create() {
 	temp->next = NULL;
 	temp->p_needtime = t_needTime;
 	temp->p_name = t_name;
-	temp->p_priority = 50 - t_needTime;
+	if (current_dispatch_algorithm == PRIORITY_ALGORITHM)
+		temp->p_priority = 50 - t_needTime; // 优先级调度
+	else
+		temp->p_priority = time_slice; // 时间片轮询
 	temp->p_cputime = 0;
 	temp->p_status = WAIT_STATE;
 	// 加入就绪队列
@@ -235,7 +296,7 @@ int priSCH() {
 		if (NULL == p_run) return 0;
 		else firstIn();
 	}
-	// 重新排序
+	// 按照优先级重新排序
 	comparePriority();
 	// 选一个
 	p_run = p_ready;
@@ -248,28 +309,39 @@ int priSCH() {
 
 // 按时间片轮转法调度进程。
 int roundSCH() {
-
-
-
-	return 0;
+	if (NULL == p_ready) {
+		// 就绪队列为空, 表示程序已经运行完, 结束运行
+		// 判断此时有没有在运行的进程
+		if (NULL == p_run) return 0;
+		else firstIn();
+	}
+	// 选一个
+	p_run = p_ready;
+	p_ready = p_ready->next;
+	// 运行
+	firstIn();
+	return 1;
 }
 
-
-//每次显示结果均为如下 5 个字段：
-//name cputime needtime priority state
-//注：
-//1．在 state 字段中，"R"代表执行态，"W"代表就绪（等待）态，"F"代表完成态。
-//2．应先显示"R"态的，再显示"W"态的，再显示"F"态的。
-//3．在"W"态中，以优先数高低或轮转顺序排队；在"F"态中，以完成先后顺序排队。
 int main() {
+	cout << "选择调度算法";
+	cout << "1.优先级调度\n2.时间片轮转调度\n其他.退出\n";
+	cin >> current_dispatch_algorithm;
 
-	// 创建5个进程
-	for (int i = 0; i < 5; i++) create();
-
-	// 优先级调度算法
-	// while(priSCH()) print();
-
-	// 时间片轮转算法
+	if (current_dispatch_algorithm == PRIORITY_ALGORITHM) {
+		for (int i = 0; i < 5; i++) create();
+		// 优先级调度算法
+		while (priSCH()) print();
+	}
+	else if (current_dispatch_algorithm == ROLL_ALGORITHM) {
+		// 时间片轮转算法
+		cout << "时间片大小";
+		cin >> time_slice;
+		for (int i = 0; i < 5; i++) create();
+		while (roundSCH()) print();
+	}
+	else return 0;
 
 	return 0;
 }
+
